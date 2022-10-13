ab363d73e6dcbbe17ea4409e440d2e38a6e7d813
Rezuan Talukder <rez-tal@outlook.com>
2022-10-13 21:43:54 +0100

    Add ValueError to primes function

--- primes.py
+++ primes.py
@@ -1,6 +1,26 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def isPrime(number):
+    if number == 1:
+        return False
+
+    for n in range (2, int(number ** 0.5) + 1):
+        if number%n == 0:
+            return False
+
+    return True
+
 def primes(number_of_primes):
     list = []
+    iteration = 0
+
+    if number_of_primes <= 0:
+        raise ValueError("Positive numbers only.")
+
+    while (len(list) < number_of_primes):
+        iteration += 1
+        if isPrime(iteration):
+            list.append(iteration)
+
     return list
